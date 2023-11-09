c08d4b3063f8cf5eacfe7b2cd42dec9c401f86d5
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2023-11-09 16:31:42 +0100

    Esercizio completo

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const yourName = (prompt("Type your name"))
+const yourSurname = (prompt("Type your surname"))
+const yourFavoriteColor = (prompt("Type your favorite colour"))
+const addNumber21 = 21
+document.getElementById("password-generator").innerHTML = "Your password is " + yourName+yourSurname+yourFavoriteColor+addNumber21;
